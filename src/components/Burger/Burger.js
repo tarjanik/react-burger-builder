ed45ccd1c0e24919476d4befcd62def216d4cd5e
Kristóf <tarjanik@gmail.com>
2018-08-05 10:43:35 +0200

    Added state for ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,15 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    const ingredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])]
+            .map((_,i) => <BurgerIngredient key={ingKey + i} type={ingKey} />);
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {ingredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
