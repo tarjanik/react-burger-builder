0d66f8604428c943587cb4a00eb67bca8e37bd83
Kristóf <tarjanik@gmail.com>
2018-08-05 16:54:33 +0200

    added checkout menu

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(key=> 
+        <li key={key}>
+            <span style={{textTransform: 'capitalize'}}>
+                {key}
+            </span>: {props.ingredients[key]}
+        </li>);
+    return (
+        <React.Fragment>
+           <h3>Your order</h3>
+           <p>Delicious burger with you selected ingredients:</p>
+           <ul>{ingredientSummary}</ul> 
+           <p>Continue to checkout?</p>
+           <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+           <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        </React.Fragment>
+    );
+}
+
+export default orderSummary;
